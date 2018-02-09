9a9c35a3259ff7497b5930c26baad2c3fbed25dd
Aleks911 <aleksrotnicki@hotmail.com>
2018-02-09 18:06:35 +0100

    dodanie elementu pojawiającego się i znikającego w zależności od wyniku i dodającego więcej punktów

--- snake.py
+++ snake.py
@@ -5,6 +5,7 @@ import time
 
 
 
+
 class Snake():
     def __init__(self):
         self.position = [100,50]
@@ -22,7 +23,7 @@ class Snake():
         if dir == "DOWN" and not self.direction == "UP":
             self.direction = "DOWN"
 
-    def move(self, foodPosition):
+    def move(self, foodPosition, starPosition):
         if self.direction == "RIGHT":
             self.position[0] += 10
         if self.direction == "LEFT":
@@ -33,13 +34,16 @@ class Snake():
             self.position[1] += 10
 
         self.body.insert(0, list(self.position))
-        if self.position == foodPos:
+        if self.position == foodPos or self.position == starPos:
             return 1
+
         else:
             self.body.pop()
             return 0
 
-    def checCollision(self):
+
+
+    def checkCollision(self):
         if self.position[0] > 490 or self.position[0] < 0:
             return 1
         elif self.position[1] > 490 or self.position[1] < 0:
@@ -68,13 +72,35 @@ class FoodSpawer():
     def setFoodOnScreen(self, b):
         self.isFoodOnScreen = b
 
+
+
+class Star():
+    def __init__(self):
+        self.position = [random.randrange(1, 50) * 10, random.randrange(1, 50) * 10]
+        self.isStarOnScreen = True
+
+    def spawnStar(self):
+        if self.isStarOnScreen == False:
+            self.position = [random.randrange(1,50)*10, random.randrange(1,50)*10]
+            self.isStarOnScreen = True
+        return self.position
+
+    def setStarOnScreen(self, b):
+        self.isStarOnScreen = b
+
+
+
+
 window = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Wow  Snake")
 fps = pygame.time.Clock()
 score = 0
-
+starImg = pygame.image.load('star.png')
 snake = Snake()
 foodSpawer = FoodSpawer()
+
+starSpawer = Star()
+
 
 def gameOver():
     screen = pygame.display.set_mode((500, 500))
@@ -94,10 +120,11 @@ def gameOver():
 
 
 
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            gameOver();
+            gameOver()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 snake.changeDirTo('RIGHT')
@@ -107,20 +134,41 @@ while True:
                 snake.changeDirTo('UP')
             if event.key == pygame.K_DOWN:
                 snake.changeDirTo('DOWN')
+
+
     foodPos = foodSpawer.spawnFood()
-    if (snake.move(foodPos) == 1):
-        score += 1
-        foodSpawer.setFoodOnScreen(False)
+    starPos = starSpawer.spawnStar()
+
+    if (snake.move(foodPos,starPos) == 1):
+
+        if (snake.getHeadPos() == foodPos):
+            foodSpawer.setFoodOnScreen(False)
+            score += 1
+
+        elif (snake.getHeadPos() == starPos):
+            starSpawer.setStarOnScreen(False)
+            score += 10
+
+
+
+
 
     window.fill(pygame.Color(225,225,225))
     for pos in snake.getBody():
         pygame.draw.rect(window, pygame.Color(0,225,0), pygame.Rect(pos[0],pos[1],10,10))
     pygame.draw.rect(window, pygame.Color(225,0,0), pygame.Rect(foodPos[0],foodPos[1],10,10))
 
-    if (snake.checCollision() == 1):
+
+    if score!=0 and score%6==0:
+        window.blit(starImg,(starPos[0],starPos[1]))
+
+
+
+    if (snake.checkCollision() == 1):
         gameOver()
+
     pygame.display.set_caption("Wow Snake | Score : " + str(score))
     pygame.display.flip()
-    fps.tick(15)
+    fps.tick(12)
 
 
